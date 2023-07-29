cecae3cd81c5314b583eaff2007e3153a1cf01f0
Muhammad Usama <musama6449@gmail.com>
2023-07-29 20:31:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+// question 1
+console.log("Hey Abu Huraira, I have started learning TYPESCRIPT" + "<br>");
+// question 2
+let myName = "M. Usama Awan";
+console.log(myName);
+// question 2
+let num1 = 534;
+let num2 = 23;
+let num3 = 12;
+let num4 = 123;
+let num5 = 45;
+let num6 = 23;
+let num7 = 754;
+let num8 = 34;
+let num9 = 41;
+let num10 = 12;
+let sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+let multiply = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+let sub = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+let divide = num1 / num5;
+let fourOperations = num3 / num2 - num6 + num5 * num10;
+console.log("sum: " + sum + " subtraction: " + sub + " Multiply: " + " Divide: " + divide + " Four Operations: " + fourOperations);
---
+++ index.ts
@@ -0,0 +1,26 @@
+// question 1
+console.log("Hey Abu Huraira, I have started learning TYPESCRIPT" + "<br>");
+
+// question 2
+let myName:string = "M. Usama Awan";
+console.log(myName);
+
+// question 3,4 
+let num1:number = 534;
+let num2:number = 23;
+let num3:number = 12;
+let num4:number = 123;
+let num5:number = 45;
+let num6:number = 23;
+let num7:number = 754;
+let num8:number = 34;
+let num9:number = 41;
+let num10:number = 12;
+let sum:number = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+let multiply:number = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+let sub:number = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+let divide:number = num1 / num5;
+let fourOperations:number = num3 / num2 - num6 + num5 * num10;
+ console.log("sum: " + sum + " subtraction: " + sub + " Multiply: "  + " Divide: " + divide + " Four Operations: " + fourOperations);
+
+
